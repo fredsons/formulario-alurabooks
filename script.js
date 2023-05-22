361c8bdf11eea6b6dc038b252e7584856b2bc603
Fredson <fredsondev@gmail.com>
2023-05-22 17:59:34 -0300

    atualizacao

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//Abordagem alternativa: utilizar a sintaxe assíncrona/await em vez das Promises encadeadas
+
+// async function consultaCep() {
+//   try {
+//     const response = await fetch('https://viacep.com.br/ws/01001250/json/');
+//     const resultado = await response.json();
+
+//     if (resultado.erro) {
+//       throw new Error('Esse CEP não existe');
+//     } else {
+//       console.log(resultado);
+//     }
+//   } catch (erro) {
+//     console.log(erro);
+//   }
+// }
+
+// consultaCep();
+
+
+
 let consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
   .then(dadosDoCEP => dadosDoCEP.json())
   .then(resultado => {
@@ -6,8 +27,10 @@ let consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
     } else
       console.log(resultado)
   })
-  .catch(erro => console.log(erro));
+  .catch(erro => console.log(erro))
+  .finally(mensagem => console.log('Processo concluido'))
 
 
 console.log(consultaCep)
+
 
